a114b536eae287320f69de2bcd561cb2995161e6
jin <jin236248@gmail.com>
2023-03-06 06:56:42 -0500

    version 0.0.4

--- src/lib/components/DragResize.react.js
+++ src/lib/components/DragResize.react.js
@@ -16,6 +16,13 @@ export default class DragResize extends Component {
             y: d.y, 
         }) 
     }
+
+    handleOnResize(e, direction, ref, delta, position) {
+        this.props.setProps({
+            w: ref.style.width,
+            h: ref.style.height,
+        })
+    }
     
     handleOnResizeStop(e, direction, ref, delta, position) {
         this.props.setProps({
@@ -26,29 +33,28 @@ export default class DragResize extends Component {
     
     render() {
         return (
-            // <div id={this.props.id}>
-                <Rnd
-                    default={{
-                        x: this.props.x, 
-                        y: this.props.y, 
-                        width: this.props.w, 
-                        height: this.props.h,
-                    }}
-                    onDragStop = {this.handleOnDragStop}
-                    onResizeStop = {this.handleOnResizeStop}
-                    disableDragging = {this.props.disableDragging}
-                    dragGrid = {this.props.dragGrid}
-                    enableResizing={this.props.enableResizing}
-                    resizeGrid = {this.props.resizeGrid}
-                    bounds={this.props.bounds}
-                    // style={this.props.style}
-                    className={this.props.className}
-                    >
-                    <div style={{height: "100%"}}>
-                        {this.props.children}
-                    </div>
-                </Rnd>
-            // </div>
+            <Rnd
+                default={{
+                    x: this.props.x, 
+                    y: this.props.y, 
+                    width: this.props.w, 
+                    height: this.props.h,
+                }}
+                onDragStop = {this.handleOnDragStop}
+                onResize = {this.handleOnResize}
+                onResizeStop = {this.handleOnResizeStop}
+                disableDragging = {this.props.disableDragging}
+                dragGrid = {this.props.dragGrid}
+                enableResizing={this.props.enableResizing}
+                resizeGrid = {this.props.resizeGrid}
+                bounds={this.props.bounds}
+                style={this.props.style}
+                className={this.props.className}
+                >
+                <div>
+                    {this.props.children}
+                </div>
+            </Rnd>
         );
     }
 }
@@ -68,5 +74,5 @@ DragResize.propTypes = {
     resizeGrid: PropTypes.array, // [1,1]
     bounds: PropTypes.string, // parent, window
     className: PropTypes.string, // square border border-0
-    // style: PropTypes.object,
+    style: PropTypes.object,
 };
